c926e62a36f9d044f3d113dc1d316ab453dd4bc8
rc3chambers <rc3chambers@gmail.com>
2018-07-29 11:50:30 -0400

    users can create a group from profile page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,26 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user! 
+  
   def new
+    @group = Group.new
+  end
+  
+  def create
+    @group = Group.new(group_params) 
+    if @group.save
+      current_user.groups << @group   
+      flash[:notice] = "You have just joined a group."
+      redirect_to profile_path(current_user) 
+    else
+     render :new
+    end
+  end
+    
+  private
+
+  def group_params
+    params.require(:group).permit(:name)
   end
 end
+
+
