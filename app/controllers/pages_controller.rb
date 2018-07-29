c02df6cb2ba7598eaffccfb21e9744bf72f24689
rc3chambers <rc3chambers@gmail.com>
2018-07-28 21:34:12 -0400

    added user profile page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def front
+    @user = current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+  resource :profile
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   root to: 'pages#front'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
